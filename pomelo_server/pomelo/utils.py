3dcfe1611d3b9c7265a52a3e4e47bdca672447c3
dannyvi <dannyvis@icloud.com>
2019-10-07 15:37:48 +0800

    grammar

--- pomelo_server/pomelo/utils.py
+++ pomelo_server/pomelo/utils.py
@@ -1,28 +1,28 @@
-from calendar import timegm
-from datetime import datetime
+# from calendar import timegm
+# from datetime import datetime
 
-from django.conf import settings
+# from django.conf import settings
 from django.utils.functional import lazy
-from django.utils.timezone import is_naive, make_aware, utc
+# from django.utils.timezone import is_naive, make_aware, utc
 
 
-#def make_utc(dt):
-#    if settings.USE_TZ and is_naive(dt):
-#        return make_aware(dt, timezone=utc)
+# def make_utc(dt):
+#     if settings.USE_TZ and is_naive(dt):
+#         return make_aware(dt, timezone=utc)
 #
-#    return dt
-#
-#
-#def aware_utcnow():
-#    return make_utc(datetime.utcnow())
+#     return dt
 #
 #
-#def datetime_to_epoch(dt):
-#    return timegm(dt.utctimetuple())
+# def aware_utcnow():
+#     return make_utc(datetime.utcnow())
 #
 #
-#def datetime_from_epoch(ts):
-#    return make_utc(datetime.utcfromtimestamp(ts))
+# def datetime_to_epoch(dt):
+#     return timegm(dt.utctimetuple())
+#
+#
+# def datetime_from_epoch(ts):
+#     return make_utc(datetime.utcfromtimestamp(ts))
 
 
 def format_lazy(s, *args, **kwargs):
